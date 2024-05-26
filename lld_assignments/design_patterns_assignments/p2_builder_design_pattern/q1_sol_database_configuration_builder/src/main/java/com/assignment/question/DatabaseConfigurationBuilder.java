b9a5fb4702d85d7c50214e5fa9a356a78c11bc17
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-05-26 12:53:36 +0530

    LLD Design Pattern Builder Pattern Implementation for a database configuration class (P2 - Assignment1)

---
+++ lld_assignments/design_patterns_assignments/p2_builder_design_pattern/q1_sol_database_configuration_builder/src/main/java/com/assignment/question/DatabaseConfigurationBuilder.java
@@ -0,0 +1,67 @@
+package com.assignment.question;
+
+@WithBuilder
+public class DatabaseConfigurationBuilder {
+    private String databaseUrl;
+    private String username;
+    private String password;
+    private int maxConnections;
+    private boolean enableCache;
+    private boolean isReadOnly;
+
+    public DatabaseConfigurationBuilder(Builder builder) {
+        this.databaseUrl = builder.databaseUrl;
+        this.username = builder.username;
+        this.password = builder.password;
+        this.maxConnections = builder.maxConnections;
+        this.enableCache = builder.enableCache;
+        this.isReadOnly = builder.isReadOnly;
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        public String databaseUrl;
+        public String username;
+        public String password;
+        public int maxConnections;
+        public boolean enableCache;
+        public boolean isReadOnly;
+
+        public Builder databaseUrl(String databaseUrl) {
+            this.databaseUrl = databaseUrl;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder maxConnections(int maxConnections) {
+            this.maxConnections = maxConnections;
+            return this;
+        }
+
+        public Builder enableCache(boolean enableCache) {
+            this.enableCache = enableCache;
+            return this;
+        }
+
+        public Builder isReadOnly(boolean isReadOnly) {
+            this.isReadOnly = isReadOnly;
+            return this;
+        }
+
+        public DatabaseConfigurationBuilder build() {
+            return new DatabaseConfigurationBuilder(this);
+        }
+    }
+}
