177ab7780e1b93aafb60eebe459f8b5f0d3f7901
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-05-26 13:04:39 +0530

    LLD Design Pattern Builder Pattern Implementation for Building Queries (P2 - Assignment2)

---
+++ lld_assignments/design_patterns_assignments/p2_builder_design_pattern/q2_sol_building_queries_builder/src/main/java/com/assignment/question/QueryBuilder.java
@@ -0,0 +1,67 @@
+package com.assignment.question;
+
+@WithBuilder
+public class QueryBuilder {
+    private String select;
+    private String from;
+    private String where;
+    private String join;
+    private String orderBy;
+    private String groupBy;
+
+    public QueryBuilder(Builder builder) {
+        this.select = builder.select;
+        this.from = builder.from;
+        this.where = builder.where;
+        this.join = builder.join;
+        this.orderBy = builder.orderBy;
+        this.groupBy = builder.groupBy;
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String select;
+        private String from;
+        private String where;
+        private String join;
+        private String orderBy;
+        private String groupBy;
+
+        public Builder select(String select) {
+            this.select = select;
+            return this;
+        }
+
+        public Builder from(String from) {
+            this.from = from;
+            return this;
+        }
+
+        public Builder where(String where) {
+            this.where = where;
+            return this;
+        }
+
+        public Builder join(String join) {
+            this.join = join;
+            return this;
+        }
+
+        public Builder orderBy(String orderBy) {
+            this.orderBy = orderBy;
+            return this;
+        }
+
+        public Builder groupBy(String groupBy) {
+            this.groupBy = groupBy;
+            return this;
+        }
+
+        public QueryBuilder build() {
+            return new QueryBuilder(this);
+        }
+    }
+}
