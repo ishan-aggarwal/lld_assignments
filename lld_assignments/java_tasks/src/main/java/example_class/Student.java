ad6eada83e84bb4201e44bff17c6fc7ee4e79bc2
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-06-24 23:47:22 +0530

    Student class example

---
+++ lld_assignments/java_tasks/src/main/java/example_class/Student.java
@@ -0,0 +1,15 @@
+package example_class;
+
+public class Student {
+
+    int age;
+    String name;
+
+    void display() {
+        System.out.println("My name is " + name + ". I am " + age + " years old");
+    }
+
+    void sayHello(String name) {
+        System.out.println(this.name + " says hello to " + name);
+    }
+}
